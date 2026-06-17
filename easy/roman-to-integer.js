bc33f0cc247e649d893f456ceb2db15ce6532e01
Tiguer04 <trustedx12@gmail.com>
2026-06-17 13:15:43 -0500

    Refactor romanToInt function to improve readability and maintainability

--- easy/roman-to-integer.js
+++ easy/roman-to-integer.js
@@ -1,75 +1,87 @@
+var romanToInt = function (s) {
+  if (/IL|IC|ID|IM|XD|XM/.test(s)) {
+    return "Pattern no allowed";
+  }
 
-var romanToInt = function(s) {
-    
-    const map = new Map();
+  const map = new Map();
 
-    map.set('I', 1); // O(1)
-    map.set('V', 5);
-    map.set('X', 10);
-    map.set('L', 50);
-    map.set('C', 100);
-    map.set('D', 500);
-    map.set('M', 1000);
-  
-    let separated = s.split("");
+  map.set("I", 1); // O(1)
+  map.set("V", 5);
+  map.set("X", 10);
+  map.set("L", 50);
+  map.set("C", 100);
+  map.set("D", 500);
+  map.set("M", 1000);
 
-    let nums = separated.map((simbol) =>{ //O(n)
-      return map.get(simbol);
-    })
+  let separated = s.split("");
 
-    let translated = [];
+  let nums = separated.map((simbol) => {
+    //O(n)
+    return map.get(simbol);
+  });
 
-    for(let i = 0; i < nums.length; i++){
+  let I = 0;
+  let V = 0;
+  let X = 0;
+  let L = 0;
+  let C = 0;
+  let D = 0;
+  let M = 0;
 
-      let subs = nums[i + 1] - nums[i];
+  for (let i = 0; i < nums.length; i++) {
+    //O(n)
 
-      if(subs > 0){
-
-        translated.push(subs);
-        nums.splice(i + 1, 1);
-
-      } else{
-        translated.push(nums[i]);
-      }
-
+    if (nums[i] == nums[i + 1] && [5, 50, 500].includes(nums[i])) {
+      return "You cannot use V, L or D twice in a row";
+    } else if (nums[i] === 1 && nums[i + 1] === 1) {
+      I++;
+    } else if (nums[i] === 10 && nums[i + 1] === 10) {
+      X++;
+    } else if (nums[i] === 100 && nums[i + 1] === 100) {
+      C++;
+    } else if (nums[i] === 1000 && nums[i + 1] === 1000) {
+      M++;
+    } else if (nums[i] === 5) {
+      V++;
+    } else if (nums[i] === 50) {
+      L++;
+    } else if (nums[i] === 500) {
+      D++;
     }
 
-    let I = 0;
-    let X = 0;
-    let C = 0;
-    let M = 0;
-
-    for(let i = 0; i < translated.length - 1; i++){
-
-      if(translated[i] == translated[i + 1] && [5, 50, 500].includes(translated[i])){
-        return "You cannot use V, L or D twice in a row";
-      } else if(translated[i] === 1 && translated[i + 1] === 1){
-
-        I++;
-
-      } else if(translated[i] === 10 && translated[i + 1] === 10){
-        X++;
-      } else if(translated[i] === 100 && translated[i + 1] === 100){
-        C++;
-      } else if(translated[i] === 1000 && translated[i + 1] === 1000){
-        M++;
-      }
-
-      if(I > 2 || X > 2 || C > 2 || M > 2){
-        return "You cannot use I, X, C or M more than three times in a row"
-      }
-
-
+    if (I > 2 || X > 2 || C > 2 || M > 2) {
+      return "You cannot use I, X, C or M more than three times in a row";
     }
 
-    const sum = translated.reduce((acc, n) => acc + n, 0);
-
-    if(sum > 3999){
-      return "There are not roman numbers above 3999";
+    if (V > 1 || L > 1 || D > 1) {
+      return "There cannot be V, L or D twice in a roman number";
     }
+  }
 
-    return sum;
+  let translated = [];
 
+  for (let i = nums.length - 1; i >= 0; i--) {
+    //O(n)
+
+    if (nums[i] > nums[i - 1]) {
+      if (nums[i - 1] === 5 || nums[i - 1] === 50 || nums[i - 1] === 500) {
+        return "V, L or D cannot be substracted";
+      }
+
+      translated.push(nums[i] - nums[i - 1]);
+      i--;
+    } else {
+      translated.push(nums[i]);
+    }
+  }
+
+  const sum = translated.reduce((acc, n) => acc + n, 0);
+
+  if (sum > 3999) {
+    return "There are not roman numbers above 3999";
+  }
+
+  return sum;
 };
 
-console.log(romanToInt("XLIX"));
+console.log(romanToInt("XVL"));
